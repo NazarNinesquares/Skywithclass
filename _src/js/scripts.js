b181bebf17e731198065efcefaafa3b50f0d70a0
NazarNinesquares <nazar@ninesquares.studio>
2023-03-15 18:23:11 +0200

    Пофіксив селектор

--- _src/js/scripts.js
+++ _src/js/scripts.js
@@ -3,7 +3,7 @@ var $ = jQuery;
 $(function() {
 	
 	$('.start-tab-nav__item').on('click', function () {
-		console.log($(this).data('tab'));
+		
 		$('.start-tab-nav__selector').attr('data-pos', $(this).data('tab'))
 	})
 })
