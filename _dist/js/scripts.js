360ec097174af9c6d897c7279f2c314cb5300f3f
NazarNinesquares <nazar@ninesquares.studio>
2023-03-16 12:37:47 +0200

    Форма з табами

--- _dist/js/scripts.js
+++ _dist/js/scripts.js
@@ -5,5 +5,7 @@ $(function() {
 	$('.start-tab-nav__item').on('click', function () {
 		
 		$('.start-tab-nav__selector').attr('data-pos', $(this).data('tab'))
+		$('.form-item').removeClass('_active');
+		$('.form-item[data-tab="'+ $(this).data('tab') +'"]').addClass('_active');
 	})
 })
