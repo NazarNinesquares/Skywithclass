09ce3098b559b0bcdf35f10b3a3344980011d3b3
NazarNinesquares <nazar@ninesquares.studio>
2023-03-17 17:20:49 +0200

    Завершив верстку

--- _dist/js/scripts.js
+++ _dist/js/scripts.js
@@ -9,13 +9,30 @@ $(function() {
 		$('.form-item[data-tab="'+ $(this).data('tab') +'"]').addClass('_active');
 	})
 
-	$('#sandbox-container .input-daterange').datepicker({
-		autoclose: true,
-		todayHighlight: true
-	});
+	if($('#sandbox-container').length) {
+		
+		$('#sandbox-container .input-daterange').datepicker({
+			autoclose: true,
+			todayHighlight: true
+		});
 
-	$('#sandbox-container input').datepicker({
-		autoclose: true,
-		todayHighlight: true
-	});
+		$('#sandbox-container input').datepicker({
+			autoclose: true,
+			todayHighlight: true
+		});
+	}
+
+	$('.loader__line').on('animationend', function() {
+		
+		$('.result__item.loader').removeClass('_active');
+		$('.result__item.content').addClass('_active');
+	})
+
+	$('.bottom-content__submit input').on('click', function (e) {
+		
+		e.preventDefault();
+		e.stopPropagation();
+		$('.result__item.content').removeClass('_active');
+		$('.result__item.thanks').addClass('_active');
+	})
 })
